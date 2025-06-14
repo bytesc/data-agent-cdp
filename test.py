6dd9f6ff2810215f81faf6756e3e2f1b370f2dfb
rcy <21010078@mail.ecust.edu.cn>
2025-06-14 21:14:11 +0800

    up date pg tp

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ tp_engine = sqlalchemy.create_engine(config_data["timeplus"])
 from agent.tools.copilot.utils.pgsql_to_tp import get_tp_table_create, pgsql_to_tp,\
     restore_column_names, get_table_name_dict
 
-print(get_table_name_dict(pg_engine))
+table_dict = get_table_name_dict(pg_engine)
 
 ans = get_tp_table_create(pg_engine)
 result = ''
@@ -20,13 +20,13 @@ print(result)
 
 from agent.tools.copilot.sql_code import get_sql_code
 
-# sql = get_sql_code("查询昵称为邓丽君的用户的邮箱归属地", None, llm, pg_engine)
-sql="""
-SELECT m.email, a.city
-FROM member_master_4_1 m
-JOIN cdp_attribute_2_1 a ON m.userId = a.userId
-WHERE a.nickName = '邓丽君';
-"""
+sql = get_sql_code("查询昵称为邓丽君的用户的邮箱归属地", None, llm, pg_engine)
+# sql="""
+# SELECT m.email, a.city
+# FROM member_master_4_1 m
+# JOIN cdp_attribute_2_1 a ON m.userId = a.userId
+# WHERE a.nickName = '邓丽君';
+# """
 # sql="""
 # SELECT *
 # FROM cdp_attribute_2_1
@@ -34,12 +34,12 @@ WHERE a.nickName = '邓丽君';
 # """
 
 
-tp_sql , r_map = pgsql_to_tp(pg_engine, sql)
+tp_sql, r_map = pgsql_to_tp(pg_engine, sql)
 print(tp_sql)
 
 from agent.tools.copilot.utils.read_db import execute_sql
 
-df = execute_sql(tp_sql.replace(';', ''))
+df = execute_sql(sql.replace(';', ''))
 
 print(df)
 
